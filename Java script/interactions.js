62ff5b609828172f4171314e764299043140efd9
Lylianmartinez <151009534+Lylianmartinez@users.noreply.github.com>
2024-01-20 16:08:01 +0100

    Add files via upload

---
+++ Java script/interactions.js
@@ -0,0 +1,78 @@
+"use strict"
+// 10 Variables liées au jeu God of War: Ragnarok
+let kratosLevel = 50;
+let kratosHealth = 100;
+let kratosRageMeter = 75;
+let kratosStrength = 95;
+
+let leviathanAxeLevel = 3;
+let leviathanAxeDamage = 120;
+let leviathanAxeMagic = "Frost";
+
+let currentLocation = "Midgard";
+let enemyCount = 15;
+let puzzlesSolved = 8;
+
+let playerGold = 500;
+
+// 2 Classes liées au jeu
+class Character {
+  constructor(name, level, health, rageMeter, strength) {
+    this.name = name;
+    this.level = level;
+    this.health = health;
+    this.rageMeter = rageMeter;
+    this.strength = strength;
+  }
+
+  attack(target) {
+    console.log(`${this.name} attacks ${target.name} with strength ${this.strength}!`);
+   
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    console.log(`${this.name} received ${damage} damage. Current health: ${this.health}`);
+  }
+}
+
+class Weapon {
+  constructor(name, level, damage, magic) {
+    this.name = name;
+    this.level = level;
+    this.damage = damage;
+    this.magic = magic;
+  }
+
+  upgrade() {
+    this.level++;
+    console.log(`${this.name} upgraded to level ${this.level}!`);
+  }
+}
+
+// 2 Fonctions liées au jeu
+function exploreLocation(location) {
+  console.log(`Exploring ${location}...`);
+  console.log(`Found ${enemyCount} enemies and ${puzzlesSolved} solved puzzles.`);
+}
+
+function earnGold(amount) {
+  playerGold += amount;
+  console.log(`Earned ${amount} gold. Total gold: ${playerGold}`);
+}
+
+// 2 messages sur la console
+console.log("Welcome to God of War: Ragnarok!");
+console.log("Get ready for an epic journey!");
+
+// Utilisation des classes et fonctions
+let kratos = new Character("Kratos", kratosLevel, kratosHealth, kratosRageMeter, kratosStrength);
+let leviathanAxe = new Weapon("Leviathan Axe", leviathanAxeLevel, leviathanAxeDamage, leviathanAxeMagic);
+
+kratos.attack(leviathanAxe);
+leviathanAxe.upgrade();
+
+exploreLocation(currentLocation);
+earnGold(200);
+
+console.log("Game over!");
